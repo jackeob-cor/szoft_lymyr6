955eabed649b8c3cad942878312fc894becac192
lymyr6 <lymyr6@SZA0030903>
2024-02-22 16:21:00 +0100

    Add project files.

---
+++ Szorzotabla/Form1.cs
@@ -0,0 +1,40 @@
+namespace Szorzotabla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+                    Button button = new();
+                    button.Text = (sor * oszlop).ToString();
+                    button.Top = sor * 40;
+                    button.Left = oszlop * 40;
+                    button.Height = 40;
+                    button.Width = 40;
+                    Controls.Add(button);
+                }
+            }
+
+
+
+
+
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
