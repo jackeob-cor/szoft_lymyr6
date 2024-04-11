c73ef40cf3c6cea07f3b584fb674b5fd32244516
BoldizsárJakab <boldi.jakab2005@gmail.com>
2024-04-11 15:09:46 +0200

    7.gyakorlat

---
+++ adatkotes/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace adatkotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            ButtonOpen = new Button();
+            ButtonSave = new Button();
+            dataGridView1 = new DataGridView();
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            populationDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            areaInSquareKmDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            countryDataBindingSource = new BindingSource(components);
+            Edit = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)countryDataBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // ButtonOpen
+            // 
+            ButtonOpen.Location = new Point(721, 38);
+            ButtonOpen.Name = "ButtonOpen";
+            ButtonOpen.Size = new Size(75, 23);
+            ButtonOpen.TabIndex = 0;
+            ButtonOpen.Text = "Megnyitás";
+            ButtonOpen.UseVisualStyleBackColor = true;
+            ButtonOpen.Click += ButtonOpen_Click;
+            // 
+            // ButtonSave
+            // 
+            ButtonSave.Location = new Point(640, 38);
+            ButtonSave.Name = "ButtonSave";
+            ButtonSave.Size = new Size(75, 23);
+            ButtonSave.TabIndex = 1;
+            ButtonSave.Text = "Mentés";
+            ButtonSave.UseVisualStyleBackColor = true;
+            ButtonSave.Click += ButtonSave_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nameDataGridViewTextBoxColumn, populationDataGridViewTextBoxColumn, areaInSquareKmDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = countryDataBindingSource;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(460, 413);
+            dataGridView1.TabIndex = 2;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // populationDataGridViewTextBoxColumn
+            // 
+            populationDataGridViewTextBoxColumn.DataPropertyName = "Population";
+            populationDataGridViewTextBoxColumn.HeaderText = "Population";
+            populationDataGridViewTextBoxColumn.Name = "populationDataGridViewTextBoxColumn";
+            // 
+            // areaInSquareKmDataGridViewTextBoxColumn
+            // 
+            areaInSquareKmDataGridViewTextBoxColumn.DataPropertyName = "AreaInSquareKm";
+            areaInSquareKmDataGridViewTextBoxColumn.HeaderText = "AreaInSquareKm";
+            areaInSquareKmDataGridViewTextBoxColumn.Name = "areaInSquareKmDataGridViewTextBoxColumn";
+            // 
+            // countryDataBindingSource
+            // 
+            countryDataBindingSource.DataSource = typeof(CountryData);
+            // 
+            // Edit
+            // 
+            Edit.Location = new Point(619, 156);
+            Edit.Name = "Edit";
+            Edit.Size = new Size(75, 23);
+            Edit.TabIndex = 3;
+            Edit.Text = "Szerkesztes";
+            Edit.UseVisualStyleBackColor = true;
+            Edit.Click += Edit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(825, 527);
+            Controls.Add(Edit);
+            Controls.Add(dataGridView1);
+            Controls.Add(ButtonSave);
+            Controls.Add(ButtonOpen);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)countryDataBindingSource).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button ButtonOpen;
+        private Button ButtonSave;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn populationDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn areaInSquareKmDataGridViewTextBoxColumn;
+        private BindingSource countryDataBindingSource;
+        private Button Edit;
+    }
+}
---
+++ adatkotes/FormEditCountry.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace adatkotes
+{
+    partial class FormEditCountry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            bindingSource1 = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.DataBindings.Add(new Binding("Text", bindingSource1, "Name", true));
+            textBox1.Location = new Point(90, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(274, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.DataBindings.Add(new Binding("Text", bindingSource1, "Population", true));
+            textBox2.Location = new Point(90, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(274, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.DataBindings.Add(new Binding("Text", bindingSource1, "AreaInSquareKm", true));
+            textBox3.Location = new Point(90, 168);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(274, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // bindingSource1
+            // 
+            bindingSource1.DataSource = typeof(CountryData);
+            // 
+            // FormEditCountry
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(457, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "FormEditCountry";
+            Text = "FormEditCountry";
+            Load += FormEditCountry_Load;
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private BindingSource bindingSource1;
+    }
+}
